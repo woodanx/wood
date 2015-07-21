422f1b2975185a060a6ef6d9e450cd331b07e6d1
woodanx <woodjandrew@hotmail.com>
2015-07-21 23:05:37 +1000

    wk4 assignment update

---
+++ Wk4 Assignment Java/starter_code/js/jquery.js
@@ -0,0 +1,36 @@
+
+$(document).ready(
+	function(){
+
+		$(".readmore").click(reveal);
+
+		function reveal(){
+		$(".hide").slideDown();		
+		}
+
+		$(".readless hide").click(unreveal);
+
+		function unreveal(){
+		$(".hide").slideUp();
+		}
+
+	
+
+
+		
+
+		
+
+
+
+
+
+
+
+}
+);
+
+
+
+
+
